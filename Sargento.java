5df20db6b9ce7f8c709a6550b1989d6e72158e18
JoaoPedrodeMeloRibeiro <160641946+JoaoPedrodeMeloRibeiro@users.noreply.github.com>
2024-04-24 09:32:25 -0300

    Create Sargento.java

---
+++ Sargento.java
@@ -0,0 +1,17 @@
+public class Sargento extends Soldado implements Defesa {
+    string especializacao;
+    int experienciaCombate;
+    
+    
+    public void mostraInfo(){
+
+        
+    }
+    public void usandoItem(){
+        
+    }
+    
+    
+    
+    
+}
