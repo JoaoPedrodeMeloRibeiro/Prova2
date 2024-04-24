744a5db4bcfb9d222c1a7290fea78fe9981927de
JoaoPedrodeMeloRibeiro <160641946+JoaoPedrodeMeloRibeiro@users.noreply.github.com>
2024-04-24 09:33:16 -0300

    Create MedicoMilitar.java

---
+++ MedicoMilitar.java
@@ -0,0 +1,14 @@
+public class MedicoMilitar extends Soldado implements Curar{
+    int anosEsperiencia;
+    int capacidadeEmergencia;
+    
+    public void mostraInfo(){
+        
+    }
+    public void usandoItem(){
+        
+    }
+
+    
+    
+}
